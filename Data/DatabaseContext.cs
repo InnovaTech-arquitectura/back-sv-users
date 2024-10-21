8c3f496a47a60b200c82205fd72fd80d1b85add1
Nicolas Quintana <140460204+Nikoquin1@users.noreply.github.com>
2024-10-21 18:06:01 -0500

    primer paso sin probar

--- Data/DatabaseContext.cs
+++ Data/DatabaseContext.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using back_sv_users.Models.Entities;
+using back_sv_Users.Models.Entities;
+using Microsoft.EntityFrameworkCore;
 using Models;
 
 namespace back_SV_users.Data
@@ -7,18 +9,21 @@ namespace back_SV_users.Data
     {
         public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
 
-        public DbSet<User> Users { get; set; }
-        public DbSet<Role> Roles { get; set; }
+        public DbSet<Models.User> Users { get; set; }
+        public DbSet<Models.Role> Roles { get; set; }
         public DbSet<Client> Clients { get; set; }
         public DbSet<Entrepreneurship> Entrepreneurships { get; set; }
+        public DbSet<Coupon> Coupons { get; set; }
+        public DbSet<CouponEntrepreneurship> CouponEntrepreneurships { get; set; }  // Agregar la tabla CouponEntrepreneurship
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<User>(entity =>
+            // Configuración de la entidad User
+            modelBuilder.Entity<Models.User>(entity =>
             {
                 entity.ToTable("user_entity", schema: "public");
                 entity.Property(u => u.Name)
-                      .IsRequired() 
+                      .IsRequired()
                       .HasMaxLength(100);
 
                 entity.Property(u => u.Email)
@@ -29,14 +34,34 @@ namespace back_SV_users.Data
                       .IsRequired();
 
                 entity.Property(u => u.RoleId)
-                .HasColumnName("role_id")
+                      .HasColumnName("role_id")
                       .IsRequired();
             });
 
-            modelBuilder.Entity<Role>().ToTable("role", schema: "public");
+            // Configuración de otras entidades
+            modelBuilder.Entity<Models.Role>().ToTable("role", schema: "public");
             modelBuilder.Entity<Client>().ToTable("client", schema: "public");
             modelBuilder.Entity<Entrepreneurship>().ToTable("entrepreneurship", schema: "public");
-        }
+            modelBuilder.Entity<Coupon>().ToTable("coupon", schema: "public");
 
+            // Configuración de la entidad CouponEntrepreneurship
+            modelBuilder.Entity<CouponEntrepreneurship>(entity =>
+            {
+                entity.ToTable("coupon_entrepreneurship", schema: "public");
+                entity.HasKey(ce => ce.Id);
+
+                // Relación con la tabla Coupon
+                entity.HasOne(ce => ce.IdCouponNavigation)
+                      .WithMany(c => c.CouponEntrepreneurships)
+                      .HasForeignKey(ce => ce.IdCoupon)
+                      .OnDelete(DeleteBehavior.Cascade);
+
+                // Relación con la tabla Entrepreneurship
+                entity.HasOne(ce => ce.IdEntrepreneurshipNavigation)
+                      .WithMany(e => e.CouponEntrepreneurships)
+                      .HasForeignKey(ce => ce.IdEntrepreneurship)
+                      .OnDelete(DeleteBehavior.Cascade);
+            });
+        }
     }
 }
---
+++ Migrations/20241021230436_AddMissingChanges.cs
@@ -0,0 +1,153 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace back_sv_users.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMissingChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "user",
+                schema: "innovatech");
+
+            migrationBuilder.EnsureSchema(
+                name: "public");
+
+            migrationBuilder.CreateTable(
+                name: "role",
+                schema: "public",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_role", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "user_entity",
+                schema: "public",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    id_card = table.Column<int>(type: "integer", nullable: false),
+                    name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    email = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    password = table.Column<string>(type: "text", nullable: false),
+                    role_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_user_entity", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "client",
+                schema: "public",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    id_user = table.Column<int>(type: "integer", nullable: false),
+                    id_card = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_client", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_client_user_entity_id_user",
+                        column: x => x.id_user,
+                        principalSchema: "public",
+                        principalTable: "user_entity",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "entrepreneurship",
+                schema: "public",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    id_user = table.Column<int>(type: "integer", nullable: false),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    logo = table.Column<string>(type: "text", nullable: false),
+                    nit = table.Column<int>(type: "integer", nullable: false),
+                    description = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_entrepreneurship", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_entrepreneurship_user_entity_id_user",
+                        column: x => x.id_user,
+                        principalSchema: "public",
+                        principalTable: "user_entity",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_client_id_user",
+                schema: "public",
+                table: "client",
+                column: "id_user");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_entrepreneurship_id_user",
+                schema: "public",
+                table: "entrepreneurship",
+                column: "id_user");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "client",
+                schema: "public");
+
+            migrationBuilder.DropTable(
+                name: "entrepreneurship",
+                schema: "public");
+
+            migrationBuilder.DropTable(
+                name: "role",
+                schema: "public");
+
+            migrationBuilder.DropTable(
+                name: "user_entity",
+                schema: "public");
+
+            migrationBuilder.EnsureSchema(
+                name: "innovatech");
+
+            migrationBuilder.CreateTable(
+                name: "user",
+                schema: "innovatech",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    Id_card = table.Column<int>(type: "integer", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Password = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_user", x => x.id);
+                });
+        }
+    }
+}
